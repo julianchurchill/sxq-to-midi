c9941c1b0996cf5d63c6a18c7fb8e632d254de8f
Julian Churchill <chewielouie03@gmail.com>
2025-12-12 18:46:44 +0000

    Added console .net app to read an midi/SXQ file and output some info about midi events

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Melanchall.DryWetMidi.Core;
+using Melanchall.DryWetMidi.Interaction;
+
+var midiFile = MidiFile.Read(args[1]);
+var tempoMap = midiFile.GetTempoMap();
+
+var count = 0;
+Console.WriteLine($"File {args[1]}");
+Console.WriteLine("TrackChunks:");
+foreach (var chunk in midiFile.GetTrackChunks())
+{
+    var timedEvents = chunk.GetTimedEvents();
+    var midiEvent = (timedEvents.SingleOrDefault(
+        t => t.Event.EventType == MidiEventType.SequenceTrackName)?.Event) as SequenceTrackNameEvent;
+    var sequenceTrackName = midiEvent?.Text.Replace("\0", "") ?? "unknown";
+    Console.WriteLine($"  Sequence/Track Name: {sequenceTrackName}");
+    foreach (var timedEvent in timedEvents)
+    {
+        Console.WriteLine($"    eventType {timedEvent.Event.EventType} event content '{timedEvent.Event}'");
+    }
+
+    var filename = $"./{count}-{sequenceTrackName}.sxq";
+    Console.WriteLine($"* Writing '{filename}'");
+    new MidiFile(chunk).Write(
+        filename,
+        overwriteFile: false,
+        format: MidiFileFormat.SingleTrack);
+    count++;
+}
+
+Console.WriteLine("TempoMap:");
+Console.WriteLine($"  {midiFile.GetTempoMap()}");
+
+Console.WriteLine("Notes:");
+foreach (var note in midiFile.GetNotes())
+{
+    var time = note.TimeAs<MetricTimeSpan>(tempoMap);
+    var length = note.LengthAs<MetricTimeSpan>(tempoMap);
+    Console.WriteLine($"  {note} at {time} with length of {length}");
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+`dotnet new console`
+`dotnet package add Melanchall.DryWetMidi`
+`dotnet run .\Programs.cs <midi or sxq file>`
