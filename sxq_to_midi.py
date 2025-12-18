aaafb37dee288566e960a4a3cf1ef8c1c75219bc
Julian Churchill <chewielouie03@gmail.com>
2025-12-18 18:13:03 +0000

    added support for half tied quarter tied 16th notes

--- sxq_to_midi.py
+++ sxq_to_midi.py
@@ -361,6 +361,7 @@ def note_length_from_rhythmic_class(rc, ppqn):
         (64, 15, 13):  (ppqn * 7) // 4, # quarter tied dotted eighth (7x16ths)
         (64, 31, 11):  (ppqn * 3) // 2, # dotted quarter
         (64, 47, 9):   (ppqn * 5) // 4, # quarter tied sixteenth (5x16ths)
+        (64, 47, 24):  (ppqn * 13) // 4,# half tied quarter tied sixteenth
         # (64, 49, 12):  (ppqn * 3) // 4, # what is a 49, 12 (31, 0C)?  A dotted quarter + a 16th? A double dotted quarter?
         (64, 63, 7):   ppqn,            # quarter
         (64, 63, 22):  ppqn * 3,        # dotted half
