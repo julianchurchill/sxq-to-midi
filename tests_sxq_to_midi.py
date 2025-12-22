e8cd9e56198e6c637c04648b39d2594233649ae9
Julian Churchill <chewielouie03@gmail.com>
2025-12-22 18:49:10 +0000

    added support for dotted whole tied dotted 8th tied 32nd notes

--- tests_sxq_to_midi.py
+++ tests_sxq_to_midi.py
@@ -53,6 +53,7 @@ class AcceptanceTests(unittest.TestCase):
         ('dottedWholeNotes4bars120bpm127velocity-ASharp'),
         ('dottedWholeTied8thTied32ndNotes4bars120bpm127velocity-ASharp'),
         ('dottedWholeTied32ndNotes4bars120bpm127velocity-ASharp'),
+        ('dottedWholeTiedDotted8thTied32ndNotes4bars120bpm127velocity-ASharp'),
         ('dottedWholeTiedDotted16thNotes4bars120bpm127velocity-ASharp'),
         ('halfNotes4bars120bpm127velocity-ASharp'),
         ('halfTied8thNotes4bars120bpm127velocity-ASharp'),
