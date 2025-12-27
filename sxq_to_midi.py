7b9183d0b4ae6277fb60933c3c7f2fcf22a49b9d
Julian Churchill <chewielouie03@gmail.com>
2025-12-27 17:53:39 +0000

    added support for whole tied 8th tied dotted 32nd notes

--- sxq_to_midi.py
+++ sxq_to_midi.py
@@ -451,6 +451,7 @@ def note_length_from_ga11_table(rc, ppqn) -> None | int:
         # expecting (64, 15, 58)
         (64, 19, 5):  (ppqn * 11) // 16,# eighth tied dotted 32nd
         (64, 19, 20): (ppqn * 43) // 16,# half tied 8th tied dotted 32nd
+        (64, 19, 35): (ppqn * 75) // 16,# whole tied 8th tied dotted 32nd
         (64, 23, 12):  (ppqn * 13) // 8,# dotted quarter tied 32nd
         (64, 23, 27):  (ppqn * 29) // 8,# dotted half tied 8th tied 32nd
         (64, 23, 42):  (ppqn * 45) // 8,# whole tied dotted quarter tied 32nd
