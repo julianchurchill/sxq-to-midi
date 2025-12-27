79fd38500bf306f278d841d49693712f51729366
Julian Churchill <chewielouie03@gmail.com>
2025-12-27 11:56:17 +0000

    added support for half tied 8th tied 64th notes

--- sxq_to_midi.py
+++ sxq_to_midi.py
@@ -453,6 +453,7 @@ def note_length_from_ga11_table(rc, ppqn) -> None | int:
         (64, 23, 42):  (ppqn * 45) // 8,# whole tied dotted quarter tied 32nd
         (64, 23, 57):  (ppqn * 61) // 8,# dotted whole tied dotted quarter tied 32nd
         (64, 27, 4):   (ppqn * 9) // 16,# eighth tied 64th
+        (64, 27, 19): (ppqn * 41) // 16,# half tied 8th tied 64th
         (64, 31, 11):  (ppqn * 3) // 2, # dotted quarter
         (64, 31, 26):  (ppqn * 7) // 2, # half tied dotted quarter
         (64, 35, 3):   (ppqn * 7) // 16,# dotted 16th tied 64th
