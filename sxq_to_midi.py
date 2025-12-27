413cb3975cd339defda0f01b066291fd71f08eaa
Julian Churchill <chewielouie03@gmail.com>
2025-12-27 11:32:35 +0000

    added support for half tied dotted 32nd notes

--- sxq_to_midi.py
+++ sxq_to_midi.py
@@ -464,6 +464,7 @@ def note_length_from_ga11_table(rc, ppqn) -> None | int:
         (64, 47, 24):  (ppqn * 13) // 4,# half tied quarter tied sixteenth
         # (64, 49, 12):  (ppqn * 3) // 4, # what is a 49, 12 (31, 0C)?  AI thinks it a dotted quarter tied 32nd (but this would be the same as 23, 12)...
         (64, 51, 1):  (ppqn * 3) // 16, # dotted 32nd
+        (64, 51, 16): (ppqn * 35) // 16,# half tied dotted 32nd
         (64, 55, 8):  (ppqn * 9) // 8,  # quarter tied 32nd
         (64, 55, 23): (ppqn * 25) // 8, # dotted half tied 32nd
         (64, 55, 38): (ppqn * 41) // 8, # whole tied quarter tied 32nd
